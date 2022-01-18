3a56c6818898f8584be3448834edf08cfe84a2a9
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2022-01-18 15:08:18 +0530

    Initial Commit

---
+++ ARFurniture/ARFurnitureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARFurnitureApp.swift
+//  ARFurniture
+//
+//  Created by KANISHK VIJAYWARGIYA on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct ARFurnitureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
